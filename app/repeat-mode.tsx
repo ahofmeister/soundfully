fd27cf8f5e17054941ba4a0f193116287c529258
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2024-12-19 14:33:44 +0100

    feat: Improve mobile design for playback controls

--- app/repeat-mode.tsx
+++ app/repeat-mode.tsx
@@ -1,64 +1,63 @@
 "use client";
 import React from "react";
-import {Button} from "@/components/ui/button";
-import {Repeat, Repeat1} from "lucide-react";
-import {usePlayback} from "@/app/playback-context";
-import {RepeatMode} from "@/utils/supabase/types";
-
+import { Button } from "@/components/ui/button";
+import { Repeat, Repeat1 } from "lucide-react";
+import { usePlayback } from "@/app/playback-context";
+import { RepeatMode } from "@/utils/supabase/types";
 
 const RepeatMode = () => {
-    const {repeatMode, setRepeatMode} = usePlayback();
+  const { repeatMode, setRepeatMode } = usePlayback();
 
-    const repeatModes: RepeatMode[] = ["none", "all", "one"];
+  const repeatModes: RepeatMode[] = ["none", "all", "one"];
 
-    const toggleRepeatMode = () => {
-        if (repeatMode) {
-            const currentIndex = repeatModes.indexOf(repeatMode);
-            const nextIndex = (currentIndex + 1) % repeatModes.length;
-            setRepeatMode(repeatModes[nextIndex]);
-        }
-    };
+  const toggleRepeatMode = () => {
+    if (repeatMode) {
+      const currentIndex = repeatModes.indexOf(repeatMode);
+      const nextIndex = (currentIndex + 1) % repeatModes.length;
+      setRepeatMode(repeatModes[nextIndex]);
+    }
+  };
 
-    const renderButton = () => {
-        switch (repeatMode) {
-            case "all":
-                return (
-                    <Button
-                        variant="secondary"
-                        size="icon"
-                        className="h-8 w-8"
-                        onClick={toggleRepeatMode}
-                    >
-                        <Repeat className="w-4 h-4 stroke-[1.5]"/>
-                    </Button>
-                );
-            case "one":
-                return (
-                    <Button
-                        variant="secondary"
-                        size="icon"
-                        className="h-8 w-8"
-                        onClick={toggleRepeatMode}
-                    >
-                        <Repeat1 className="w-4 h-4 stroke-[1.5]"/>
-                    </Button>
-                );
-            case "none":
-            default:
-                return (
-                    <Button
-                        variant="ghost"
-                        size="icon"
-                        className="h-8 w-8"
-                        onClick={toggleRepeatMode}
-                    >
-                        <Repeat className="w-4 h-4 stroke-[1.5]"/>
-                    </Button>
-                );
-        }
-    };
+  const renderButton = () => {
+    switch (repeatMode) {
+      case "all":
+        return (
+          <Button
+            variant="secondary"
+            size="icon"
+            className="h-8 w-8"
+            onClick={toggleRepeatMode}
+          >
+            <Repeat className="size-6 stroke-[1.5]" />
+          </Button>
+        );
+      case "one":
+        return (
+          <Button
+            variant="secondary"
+            size="icon"
+            className="h-8 w-8"
+            onClick={toggleRepeatMode}
+          >
+            <Repeat1 className="size-6 stroke-[1.5]" />
+          </Button>
+        );
+      case "none":
+      default:
+        return (
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-8 w-8"
+            onClick={toggleRepeatMode}
+          >
+            <Repeat className="size-6 stroke-[1.5]" />
+          </Button>
+        );
+    }
+  };
 
-    return <>{renderButton()}</>;
+  return <>{renderButton()}</>;
 };
 
 export default RepeatMode;
